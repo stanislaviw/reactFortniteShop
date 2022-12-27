03618d03da207f5f374bf9038f13eb6c048d640a
stanislaviw <slavi8ok@gmail.com>
2022-12-27 20:12:58 +0200

    added contacts page

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -3,15 +3,18 @@ function Contacts() {
     return (
         <main className="container">
             <div className="contacts-container">
-                <h2 className="contacts-title">
-                    Contacts
-                </h2>
-                <div>
-                    <a className="contacts-link">GitHub</a>
-                    <a className="contacts-link">LinkedIn</a>
-                    <a className="contacts-link">Mail</a>
-                    <a className="contacts-link">Telegram</a>
-                    <a className="contacts-link">Instagram</a>
+                <h3 className="contacts-title">
+                    You can find us here:
+                </h3>
+                <div className="links">
+                    <a href="https://github.com/stanislaviw/reactFortniteShop" className="contacts-link">GitHub</a>
+                    <a href="https://www.linkedin.com" className="contacts-link">LinkedIn</a>
+                    <a href="https://www.google.com/intl/ru/gmail" className="contacts-link">Mail</a>
+                    <a href="https://web.telegram.org/" className="contacts-link">Telegram</a>
+                    <a href="https://www.instagram.com/" className="contacts-link">Instagram</a>
+                    <a href="https://twitter.com/" className="contacts-link">Twitter</a>
+                    <a href="https://www.facebook.com/" className="contacts-link">Facebook</a>
+                    <a href="https://store.steampowered.com/?l=russian" className="contacts-link">Steam</a>
                 </div>
             </div>
         </main>
