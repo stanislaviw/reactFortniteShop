adceb755a4a3188b988562f35bc887d3b017d469
stanislaviw <slavi8ok@gmail.com>
2023-01-07 09:19:59 +0200

    bugfix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
+import {Link} from 'react-router-dom';
+
 function Footer () {
     return (
         <footer className="page-footer #6a1b9a purple darken-3">
           <div className="footer-copyright">
             <div className="container">
+              <div className="footer-links">
+                <Link className="footer-links-item" to="/">Shop</Link>
+                <Link className="footer-links-item" to="/about">About</Link>
+                <Link className="footer-links-item" to="/contacts">Contacts</Link>
+              </div>
               <span>© {new Date().getFullYear()} Stanislav Ivanov</span>
               <a className="grey-text text-lighten-4 right" href="https://github.com/stanislaviw/reactFortniteShop">Repositorie</a>
             </div>
